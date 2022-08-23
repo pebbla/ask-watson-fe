8d9d589b5557060a3c0706dc1454b680de531610
hanslelee <hssarah13@gmail.com>
2022-08-24 00:09:04 +0900

    side-bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 
 import React from "react";
 import SideBar from "./components/common/sidebar/SideBar";
-import { BrowserRouter, Routes, Route, Switch} from "react-router-dom";
-// import { Route, Switch } from 'react-router';
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Cafe from "./pages/cafe/Cafe";
 import "./App.css";
 
 function App() {
 
   return (
-    <div>
+    <BrowserRouter>
       <SideBar />
-      <Cafe />
-    </div>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Cafe />} />
-    //     {/* <Route path="users/*" element={<Users />} /> */}
-    //   </Routes>
-    //   <Route path="/" exact={true} component={Cafe} />
-      
-    // </BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Cafe />} />
+        <Route path="/cafe" element={<Cafe />} />
+      </Routes>
+    </BrowserRouter>
   );
-
 
 }
 
