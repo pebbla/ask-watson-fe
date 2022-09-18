17e92d8b48a7c04d7ae3fb192918da791670453f
hanslelee <hssarah13@gmail.com>
2022-09-18 17:31:06 +0900

    카페, 테마 수정 안된것 먼저 보기

--- src/components/cafe/CafeList.js
+++ src/components/cafe/CafeList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import Cafe from "./Cafe.js";
 import "./CafeList.scss"
 
@@ -9,11 +11,26 @@ function CafeList({searchWord}) {
     };
 
     const [cafeList, setCafeList] = useState([])
+    const [isSortedByUpdate, setSortByUpdate] = useState(false)
 
-    useEffect(() => {getCafeList()}, [])
+    useEffect(() => {getCafeList()}, [isSortedByUpdate])
 
     async function getCafeList() {
-        await axios
+        if(isSortedByUpdate) {
+            console.log("sortbyupdate true");
+            await axios
+            .get("http://localhost:8080/v1/admin/cafes?searchWord="+searchWord+"&sortByUpdateYn=true", config)
+            .then(response => {
+                var data = response.data['data']
+                setCafeList(data);
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error("ERROR: " + error);
+            })
+        } else {
+            console.log("sortbyupdate false");
+            await axios
             .get("http://localhost:8080/v1/admin/cafes?searchWord="+searchWord, config)
             .then(response => {
                 var data = response.data['data']
@@ -24,17 +41,28 @@ function CafeList({searchWord}) {
             .catch((error) => {
                 console.error("ERROR: " + error);
             })
+        }
+        
     };
 
-    
-
     return <div className="cafe-list-layout">
+        {isSortedByUpdate
+            ? <div className="sort-btn" onClick={() => setSortByUpdate(false)}>
+                <FontAwesomeIcon className="faCheckCircle" icon={faCheckCircle} />
+                <h6>수정 안된것 먼저 보기</h6>
+            </div>
+            : <div className="sort-btn" onClick={() => setSortByUpdate(true)}>
+                <div className="circle" ></div>
+                <h6>수정 안된것 먼저 보기</h6>
+            </div>}
+        <div className="cafe-list">
         {cafeList.map((cafe) => {
             return <Cafe 
                 key = {cafe.id}
                 cafe = {cafe}
             />
         })}
+        </div>
     </div>
 }
 
